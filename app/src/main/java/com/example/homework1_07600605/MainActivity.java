c0d77d68e9be91196b435e9024e7bebd16f598c9
kamolchanok hengsathanakul <kamolchanok1533@gmail.com>
2020-08-29 02:00:49 +0700

    Initial commit

---
+++ app/src/main/java/com/example/homework1_07600605/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.homework1_07600605;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
